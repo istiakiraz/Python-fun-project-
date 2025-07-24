328e3e7359e1391f6a7f013db125df54ec09ccd8
Istiak Mahmud <istiakiraz@gmail.com>
2025-07-24 23:06:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
-
+drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
 index_y = 0
 
